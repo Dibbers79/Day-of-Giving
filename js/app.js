abafa681e92dc465b607caf7dd4254a61aa785b7
Thomas Dybsky <10704392+Dibbers79@users.noreply.github.com>
2018-03-16 14:14:34 -0500

    Cleaned up app.js
    reordered and merged some

--- js/app.js
+++ js/app.js
@@ -1,46 +1,74 @@
+window.onload = function() {
+	move();
+	move2();
+}
+function move() {
+	var elem = document.getElementById("myBar"); 
+	var width = 10;
+	var id = setInterval(frame, 10);
+	function frame() {
+		if (width >= 87) {
+			clearInterval(id);
+		} else {
+			width++; 
+			elem.style.width = width + '%'; 
+			elem.innerHTML = width * 1 + '%';
+		}
+	}
+}
+function move2() {
+	var elem = document.getElementById("myBar2"); 
+	var width = 10;
+	var id = setInterval(frame, 10);
+	function frame() {
+		if (width >= 85) {
+			clearInterval(id);
+		} else {
+			width++; 
+			elem.style.width = width + '%';
+			elem.innerHTML = 1925 + width + 's';
+		}
+	}
+}
 $(document).ready(function(){
-	$('.shivasurya-count').each(function () { //find class shivasurya-count, start function
-		$(this).prop('Counter',0).animate({ //target itself, and create a new property: var Counter and zero, start animate function
-			Counter: $(this).text() //set value of var counter to passed # and change text to?
-		}, { //end one start another function
+	$('.shivasurya-count').each(function () {
+		$(this).prop('Counter',0).animate({
+			Counter: $(this).text()
+		}, {
 			duration: 2000,
 			easing: 'swing',
-			step: function (now) { //call a loop func, now? self start?
-				$(this).text(Math.ceil(now)); //set this var to a rounded number? now?
+			step: function (now) {
+				$(this).text(Math.ceil(now));
 			}
 		});
 	});
-});
-$('.shivasurya-count').click(function(){
-	$('.shivasurya-count').each(function () { //find class shivasurya-count, start function
-		$(this).prop('Counter',0).animate({ //target itself, and create a new property: var Counter and zero, start animate function
-			Counter: $(this).text() //set value of var counter to passed # and change text to?
-		}, { //end one start another function
-			duration: 2000,
-			easing: 'swing',
-			step: function (now) { //call a loop func, now? self start?
-				$(this).text(Math.ceil(now)); //set this var to a rounded number? now?
-			}
-		});
-	});
-});
-var clock;
-$(document).ready(function() {
+	var clock;
 	var currentDate = new Date();
-	var futureDate  = new Date("April 20, 2017 12:00 AM");
+	/* 2 Month Count Down */
+	/*var futureDate  = new Date("April 20, 2017 12:00 AM");
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 	clock = $('#Long-Count').FlipClock(diff, {
 		clockFace: 'DailyCounter',
 		countdown: true
-	});
-});
-var clock;
-$(document).ready(function() {
-	var currentDate = new Date();
+	});*/
+	/* 24 Hour Count Down */
 	var futureDate  = new Date("April 21, 2017 12:00 AM");
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 	clock = $('#Short-Count').FlipClock(diff, {
 		countdown: true
+	});
+});
+$('.shivasurya-count').click(function(){
+	$('.shivasurya-count').each(function () {
+		$(this).prop('Counter',0).animate({
+			Counter: $(this).text()
+		}, {
+			duration: 2000,
+			easing: 'swing',
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
 	});
 });
 $(document).foundation();
